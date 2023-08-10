6d9ad9d6655cf1b6fbb471c5dd7e7e6cf30efe32
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-08-10 23:28:52 +0530

    component refcatoring and userEvent vs FireEvent

---
+++ react-testing/src/FormBox.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+interface FormBoxProps {
+    labelText: React.ReactNode;
+    value: string;
+    onChange(event: React.ChangeEvent<HTMLInputElement>): void;
+    placeholder: string
+  }
+  
+ export default function FormBox ({labelText, value, onChange, placeholder} : FormBoxProps) {
+    return (
+      <div className="form-box">
+        <label htmlFor='search'>{labelText}</label>
+        <input placeholder={placeholder} id="search" type="text" value = {value} onChange={onChange} />
+      </div>
+    )
+  }
