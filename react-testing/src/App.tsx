878f1c49877d0a09b45336865a52c7b85112d98d
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-08-10 22:58:30 +0530

    Add. ByAll test cases

--- react-testing/src/App.tsx
+++ react-testing/src/App.tsx
@@ -8,18 +8,22 @@ interface FormBoxProps {
   labelText: React.ReactNode;
   value: string;
   onChange(event: React.ChangeEvent<HTMLInputElement>): void;
+  placeholder: string
 }
 
-function FormBox ({labelText, value, onChange} : FormBoxProps) {
+function FormBox ({labelText, value, onChange, placeholder} : FormBoxProps) {
   return (
     <div className="form-box">
       <label htmlFor='search'>{labelText}</label>
-      <input placeholder='color' id="search" type="text" value = {value} onChange={onChange} />
+      <input placeholder={placeholder} id="search" type="text" value = {value} onChange={onChange} />
     </div>
   )
 }
+
+
 function App() {
   const [color, setColor] = useState("");
+  const [hobby, setHobby] = useState("");
   const [user, setUser] = useState<User | null>(null);
 
   useEffect(() => {
@@ -34,16 +38,37 @@ function App() {
     setColor(event.target.value)
   }
 
+  const handleHobbyChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setHobby(event.target.value)
+  }
+
   return (
     <div className="centered-container">
       {user ? <p>Username: {user.name}</p> : null}
       <FormBox 
     labelText={"Add your favourite color:"} 
     value={color} 
-    onChange={handleColorChange} />
+    onChange={handleColorChange}
+    placeholder={"color"} />
     <h2>Your favorite color is: {color}</h2>
+
+    <FormBox 
+    labelText={"Add your hobby:"} 
+    value={hobby} 
+    onChange={handleHobbyChange}
+    placeholder={"hobby"} />
+     <h2>Your hobby is: {hobby}</h2>
+     
+     <FormBox 
+    labelText={"Add your hobby:"} 
+    value={hobby} 
+    onChange={handleHobbyChange} 
+    placeholder={"hobby"}/>
+     <h2>Your hobby is: {hobby}</h2>
+     
     </div>
 
+    
     
   );
 }
