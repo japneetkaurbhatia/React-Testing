d024cd2b7203e75c37bbb3c7d8f435a635d8d8c9
Japneet Kaur Bhatia <56752427+japneetbhatia@users.noreply.github.com>
2023-08-10 23:56:32 +0530

    pokemon component

---
+++ react-testing/src/Pokemon.tsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import FormBox from './FormBox';
+
+const pokemonApiUrl = 'https://pokeapi.co/api/v2';
+
+type Ability = {
+  ability: {
+    name: string;
+    url: string;
+  };
+  is_hidden: boolean;
+  slot: number;
+};
+
+function Pokemon() {
+  const [pokemonName, setPokemonName] = useState('');
+  const [pokemonAbilities, setPokemonAbilities] = useState<Ability[]>([]);
+  const [error, setError] = useState("null");
+
+  async function handleFetch(event: React.MouseEvent) {
+    let result;
+    try {
+      result = await axios.get(`${pokemonApiUrl}/pokemon/${pokemonName}`);
+      setPokemonAbilities(result.data.abilities);
+    } catch (error) {
+      setPokemonAbilities([]);
+    //   setError(error);
+    }
+  }
+
+  function handleChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setPokemonName(event.target.value);
+  }
+
+  return (
+    <div>
+      <FormBox labelText={"Pokemon name:"} value={pokemonName} onChange={handleChange} placeholder={'pokemon'}  />
+      <button type="button" onClick={handleFetch}>
+        Fetch Pokemon abilities
+      </button>
+      {error && <span>Something went wrong...</span>}
+      <ul>
+        {pokemonAbilities.map((ability) => (
+          <li key={ability.ability.name}>
+            <a href={ability.ability.url}>{ability.ability.name}</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Pokemon;
